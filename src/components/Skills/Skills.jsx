2a2a092512ecbf7f0a4d276f2addaf3a170fe5cd
Kipleting Edwin <kipletingedwin4@gmail.com>
2023-10-06 16:25:46 +0300

    add animation to my skills

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,45 +1,47 @@
 // import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import styles from './Skills.module.css'
-import { faCss3, faGitAlt, faHtml5, faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styles from "./Skills.module.css";
+import {
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faReact,
+} from "@fortawesome/free-brands-svg-icons";
 
 function Skills() {
   return (
-    <section id='skills' className={styles.skills}>
-        <h2> My Skills </h2>
-        <div className={styles.stageCube}> 
-        <div className={styles.cubespiner}> 
-        <div className={styles.face1}>
-            <FontAwesomeIcon icon={faHtml5} color='#DD0031' />
+    <section id="skills" className={styles.skills}>
+      <h2> My Skills </h2>
+      <div className={styles.stageCube}>
+        <div className={styles.cubespiner}>
+          <div className={styles.face1}>
+            <FontAwesomeIcon icon={faHtml5} color="#DD0031" />
+          </div>
+
+          <div className={styles.face2}>
+            <FontAwesomeIcon icon={faCss3} color="#F06529" />
+          </div>
+
+          <div className={styles.face3}>
+            <FontAwesomeIcon icon={faReact} color="#28A4D9" />
+          </div>
+
+          <div className={styles.face4}>
+            <FontAwesomeIcon icon={faJsSquare} color="#5ED4F4" />
+          </div>
+
+          <div className={styles.face5}>
+            <FontAwesomeIcon icon={faGitAlt} color="#EED81D" />
+          </div>
+
+          <div className={styles.face6}>
+            <FontAwesomeIcon icon={faReact} color='#EC4D28' />
          </div>
-
-         <div className={styles.face2}>
-            <FontAwesomeIcon icon={faCss3} color='#F06529' />
-         </div>
-
-         <div className={styles.face3}>
-            <FontAwesomeIcon icon={faReact} color='#28A4D9' />
-         </div>
-
-         <div className={styles.face4}>
-            <FontAwesomeIcon icon={faJsSquare} color='#5ED4F4' />
-         </div>
-
-         <div className={styles.face5}>
-            <FontAwesomeIcon icon={faGitAlt} color='#EED81D' />
-         </div>
-
-         {/* <div className={styles.face6}>
-            <FontAwesomeIcon icon={faRub} color='#EC4D28' />
-         </div> */}
-
- </div>
-
         </div>
-
+      </div>
     </section>
-    
-  )
+  );
 }
 
-export default Skills
+export default Skills;
