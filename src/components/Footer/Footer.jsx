c7bfecba26320da761278f1b345cb490f8b77202
Kipleting Edwin <kipletingedwin4@gmail.com>
2023-10-14 12:35:51 +0300

    remove lis from anchor tags

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,31 +1,46 @@
 // import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faGitAlt, faLinkedin, faTwitter} from "@fortawesome/free-brands-svg-icons"
-import styles from './Footer.module.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGitAlt,
+  faLinkedin,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import styles from "./Footer.module.css";
 
 function Footer() {
   return (
     <footer className={styles.myFooter}>
-        <h3> This is my footer </h3>
-        <ul>
-            <li>
-            <a
-              href="https://github.com/KipletingEdwin" target="_blank" rel="noopener noreferrer"  > <FontAwesomeIcon icon={faGitAlt} /></a>
-            </li>
-
-            <li>
-            <a
-              href="https://www.linkedin.com/in/kipleting-edwin-25a513120/" target="_blank" rel="noopener noreferrer"  > <FontAwesomeIcon icon={faLinkedin} /></a>
-            </li>
-
-            <li>
-            <a
-              href="https://twitter.com/carmago_jose_" target="_blank" rel="noopener noreferrer"  > <FontAwesomeIcon icon={faTwitter} /></a>
-            </li>
-        </ul>
-    </footer>
+      <h3> This is my footer </h3>
+      <ul>
+        <a
+          href="https://github.com/KipletingEdwin"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
     
-  )
+          <FontAwesomeIcon icon={faGitAlt} />
+        </a>
+
+        <a
+          href="https://www.linkedin.com/in/kipleting-edwin-25a513120/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        
+          <FontAwesomeIcon icon={faLinkedin} />
+        </a>
+
+        <a
+          href="https://twitter.com/carmago_jose_"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        
+          <FontAwesomeIcon icon={faTwitter} />
+        </a>
+      </ul>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
